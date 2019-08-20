9ea6e335ccdbea876c393842ede2de729e179d73
gakuto <mydance365@gmail.com>
2019-08-21 08:35:43 +0900

    20190821 mac

--- resources/views/lesson.blade.php
+++ resources/views/lesson.blade.php
@@ -23,16 +23,23 @@
                         <h3>Zabu&Eriレッスン</h3>
                     </div>
                     <div class="lesson">
-                        <h4 class="h4-title"><i class="fas fa-circle h4-title-lesson-before"></i>レギュラーclass</h4>
+                        <h4 class="h4-title-lesson"><i class="fas fa-circle h4-title-lesson-before"></i>レギュラーclass</h4>
                         <div class="lesson-description">
                             <p>基本のステップを練習したあと、いくつか技を組み合わせて踊るオープンクラス</p>
                         </div>
                         <div class="detail">
-                            <ul>
-                                <li><p>日時：毎週月曜日 21:15〜22:45</p></li>
-                                <li><p>料金：1名で参加2,000円　男女ペアで参加ペアで3,000円</p></li>
-                                <li><p>場所：新宿ﾏｲｽﾀｼﾞｵ5B　http://shinjukustudio.jp/access/index.html</p></li>
-                            </ul>
+                            <div class="detail-css">
+                              <p><i class="fas fa-chevron-circle-right"></i>日時</p>
+                              <p>毎週月曜日 21:15〜22:45</p>
+                            </div>
+                            <div class="detail-css">
+                              <p><i class="fas fa-chevron-circle-right"></i>料金</p>
+                              <p>2,000円（リードとフォロワーのペアで3,000円）</p>
+                            </div>
+                            <div class="detail-css">
+                              <p><i class="fas fa-chevron-circle-right"></i>場所</p>
+                              <p>新宿ﾏｲｽﾀｼﾞｵ5B<br>（http://shinjukustudio.jp/access/index.html）</p>
+                            </div>
                             <div class="map">
                                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.2409129583043!2d139.69857506550247!3d35.695688736785904!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188cd63a036d8b%3A0xc711fed0636fd85b!2z5paw5a6_44Oe44Kk44K544K_44K444Kq!5e0!3m2!1sja!2sjp!4v1566286922106!5m2!1sja!2sjp" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
                             </div>
@@ -44,9 +51,21 @@
                             <p>基本のステップと簡単なルーティーンをひらすらひたすら練習する初心者向け、少人数なクラス</p>
                         </div>
                         <div class="detail">
-                            <p>日時：金曜日 21:15〜22:45<br>（毎月1回のみ。カレンダー要確認）</p>
-                            <p>料金：体験2回まで1,500円</p>
-                            <p>場所：三鷹 Dance StudioemotionS　http://dance-studio-emotions.com/</p>
+                            <div class="detail-css">
+                                <p><i class="fas fa-chevron-circle-right"></i>日時</p>
+                                <p>金曜日 21:15〜22:45<br>（毎月1回のみ。カレンダー要確認）</p>
+                            </div>
+                            <div class="detail-css">
+                                <p><i class="fas fa-chevron-circle-right"></i>料金</p>
+                                <p>体験2回まで1,500円</p>
+                            </div>
+                            <div class="detail-css">
+                                <p><i class="fas fa-chevron-circle-right"></i>場所</p>
+                                <p>三鷹 Dance StudioemotionS<br>（http://dance-studio-emotions.com）</p>
+                            </div>
+                            <div class="map">
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d103703.21101161434!2d139.48923889459718!3d35.68376428460022!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018efb022342975%3A0x459c377bc849d328!2semotionS+Dance+Studio!5e0!3m2!1sja!2sjp!4v1566297287758!5m2!1sja!2sjp" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                            </div>
                         </div>
                     </div>
                     <div class="lesson">
@@ -57,6 +76,10 @@
                         <div class="detail">
                             <p>日時：金曜日 19:30〜20:45<br>（基本隔週です。カレンダー要確認）</p>
                             <p>料金：1500円</p>
+                            <p>場所：Raise up studio（https://www.raiseup-studio.com）</p>
+                            <div class="map">
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3243.7385745704214!2d139.6683914152859!3d35.609514741509216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018f51f8a2c21c5%3A0xf1117007f50d3474!2zUkFJU0UgVVAgU1RVRElPIOiHqueUseOBjOS4mCDjg6zjg7Pjgr_jg6vjgrnjgr_jgrjjgqo!5e0!3m2!1sja!2sjp!4v1566298447320!5m2!1sja!2sjp" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                            </div>
                         </div>
                     </div>
                     <div class="lesson">
@@ -69,6 +92,9 @@
                             <p><Time Table>①10:00-11:00 初中級<br>②11:00-12:00 入門<br>③12:00-13:00 練習会</p>
                             <p>場所：Roppongi Dance Lab.六本木エルビル4F　https://lab.dance</p>
                             <p>料金：2000円＋場所代500円<br>※①、②片方受講の方も、両方受講の方も同額です。<br>※練習会のみ参加の方は場所代500円のみでOK！</p>
+                            <div class="map">
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.535716503825!2d139.7297439152872!3d35.6638089385346!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188b7808340001%3A0x6d3c7b09d208deaa!2sRoppongi+Dance+Lab!5e0!3m2!1sja!2sjp!4v1566298606480!5m2!1sja!2sjp" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                            </div>
                         </div>
                     </div>
                     <p>Zabu&Eriレッスンの詳細はFacebookページをご覧ください。</p>
