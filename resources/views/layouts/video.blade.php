e8d58da0c2f37ba2625be540970b8a748968095d
gakuto <mydance365@gmail.com>
2019-09-04 23:19:06 +0900

    20190904 mac3

--- resources/views/layouts/video.blade.php
+++ resources/views/layouts/video.blade.php
@@ -173,5 +173,13 @@
           objectFitImages('img.object-fit-img');
         </script>
     </body>
-
+    <script>
+    function init() {
+    var vidDefer = document.getElementsByTagName('iframe');
+    for (var i=0; i<vidDefer.length; i++) {
+    if(vidDefer[i].getAttribute('data-src')) {
+    vidDefer[i].setAttribute('src',vidDefer[i].getAttribute('data-src'));
+    } } }
+    window.onload = init;
+    </script>
 </html>
